18d8a7109bc2cf61ba53ca829b65aff627ca7977
dknox-jr <dknox.jr@gmail.com>
2015-01-17 09:15:47 -0800

    number of players selection and token selection working.

--- monopoly/gameplay.py
+++ monopoly/gameplay.py
@@ -11,62 +11,67 @@ def number_of_players():
     Pick a number 2 - 4 and press Enter.
 
     """
-    players = raw_input(">>>")
+    num_players = raw_input(">>>")
     numbers = ["2", "3", "4"]
-    if players in numbers:
-        print "We'll get a game set up for " + str(players) + """ players.
+    if num_players in numbers:
+        print "We'll get a game set up for " + str(num_players) + """ players.
 
 
         """
-        choose_tokens(number)
-        return players
+        choose_tokens(num_players)
+        return num_players
     else:
         number_of_players()
 
-number = number_of_players()
 
 tokens = ["dog", "car", "iron", "thimble"]
 
 
-def choose_tokens(number):
-    print "Player One, please choose a token from this list " + str(tokens)
+def choose_tokens(num_players):
+    print "Player One, please choose a token from this list: " + str(tokens)
     choice1 = raw_input(">>>")
     if choice1 in tokens:
         index1 = tokens.index(choice1)
-
-        choose_tokens2(index1, number)
-        return index1
+        tokens.pop(index1)
+        choose_tokens2(choice1, num_players)
+        return choice1
     else:
         print "You didn't enter an eligible token..."
-        choose_tokens(number)
+        choose_tokens(num_players)
 
 
-def choose_tokens2(index1, number):
-    print "Player One chose " + str(tokens.pop(index1)) + ".  Player Two, please choose a token: " + str(tokens)
+def choose_tokens2(choice1, num_players):
+    print "Player One chose " + str(choice1) + ".  Player Two, please choose a token: " + str(tokens)
     choice2 = raw_input(">>>")
     if choice2 in tokens:
         index2 = tokens.index(choice2)
-        if number > 2:
-            choose_tokens3(index2, number)
-            return index2
+        tokens.pop(index2)
+        if int(num_players) > 2:
+            choose_tokens3(choice2, num_players)
+            return choice2
+        else:
+            print "Player Two chose " + str(choice2)
     else:
         print "That's not an option..."
-        choose_tokens2(index1, number)
+        choose_tokens2(choice1, num_players)
 
 
-def choose_tokens3(index2, number):
-    print "Player Two chose " + str(tokens.pop(index2)) + ".  Player Three, which do you want? " + str(tokens)
+def choose_tokens3(choice2, num_players):
+    print "Player Two chose " + str(choice2) + ".  Player Three, which do you want? " + str(tokens)
     choice3 = raw_input(">>>")
     if choice3 in tokens:
         index3 = tokens.index(choice3)
-        if number > 3:
-            choose_tokens4(index3)
-            return index3
+        tokens.pop(index3)
+        if int(num_players) > 3:
+            choose_tokens4(choice3)
+            return choice3
+        else:
+            print "Player Three chose " + str(choice3)
     else:
         print "Sorry..."
-        choose_tokens3(index2, number)
+        choose_tokens3(choice2, num_players)
 
 
-def choose_tokens4(index3):
-    print "Player Three chose " + str(tokens.pop(index3)) + ".  Player Four, you get the " + str(tokens) + " token."
+def choose_tokens4(choice3):
+    print "Player Three chose " + str(choice3) + ".  Player Four, you get the " + str(tokens) + " token."
 
